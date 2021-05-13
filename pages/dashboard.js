71b4dfdb7bbf374a55f60e5fd9dc1c2422bd129f
matt1light <matt1light@gmail.com>
2021-05-13 17:38:13 -0400

    Add all pages

---
+++ pages/dashboard.js
@@ -0,0 +1,8 @@
+import Head from 'next/head';
+
+export const Dashboard = () => {
+
+}
+
+
+export default Dashboard;
---
+++ pages/pools/index.js
@@ -0,0 +1,9 @@
+
+import Head from 'next/head';
+
+export const Pools = () => {
+
+}
+
+
+export default Pools;
