97e1194fd5d614e0d98600684fac0094b8ebd458
matt1light <matt1light@gmail.com>
2021-05-17 21:46:41 -0400

    tailwind config editing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	darkMode: false,
-	theme: {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
 		extend: {
 			backgroundImage: theme => ({
 				'water': "url('/pexels-tembela-bohle-1029545.jpg')",
 				'beach': "url('/pexels-catalin-goia-1478765.jpg')"
 			})
 		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/forms')
-	]
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
