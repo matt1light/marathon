956bdf51b2e4772a69150da6a8b48132cdf74006
matt1light <matt1light@gmail.com>
2021-05-18 08:39:14 -0400

    Add navigation to the library

--- components/Layout/Body.js
+++ components/Layout/Body.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 export const Body = (props) => {
     const {children} = props;
 
+
     return <body className="bg-beach bg-cover">
         {/** Navbar */}
         <Navbar/>
