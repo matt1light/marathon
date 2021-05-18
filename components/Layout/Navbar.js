94e5d33af7768534b3a7711ed3c872f35ca55bbf
matt1light <matt1light@gmail.com>
2021-05-18 08:46:37 -0400

    Add container to large navbar

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -49,29 +49,31 @@ export const Navbar = () => {
                     CONNECT
                 </button>
             </div>
-            <div className="hidden md:flex px-6 py-4 flex-row justify-between items-center fixed w-full h-20 top-0 bg-gray-100">
-                <div className="flex flex-row justify-start space-x-2">
-                    {/* <button className="h-8 w-8 rounded-full hover:bg-gray-300" onClick={handleOpenDrawer}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button> */}
-                    <button className="h-8 w-8 rounded-full">
-                        <img className="h-7 w-7" src={"/turtle.png"} alt="turtle logo"/>
+            <div className="hidden md:flex h-20 top-0 bg-gray-100 fixed w-full justify-center">
+                <div className="px-6 py-4 flex flex-row justify-between items-center container">
+                    <div className="flex flex-row justify-start space-x-2">
+                        {/* <button className="h-8 w-8 rounded-full hover:bg-gray-300" onClick={handleOpenDrawer}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </button> */}
+                        <button className="h-8 w-8 rounded-full">
+                            <img className="h-7 w-7" src={"/turtle.png"} alt="turtle logo"/>
+                        </button>
+                        {
+                            paths.map((item) => {
+                                return <Link href={item.path}>
+                                    <button className="hover:underline" key={item.path}>
+                                        {item.name}
+                                    </button>
+                                </Link>
+                            })
+                        }
+                    </div>
+                    <button className="rounded-full bg-green-700 px-5 py-1 text-white flex-row justify-center items-center hover:bg-green-600">
+                        CONNECT
                     </button>
-                    {
-                        paths.map((item) => {
-                            return <Link href={item.path}>
-                                <button className="hover:underline" key={item.path}>
-                                    {item.name}
-                                </button>
-                            </Link>
-                        })
-                    }
                 </div>
-                <button className="rounded-full bg-green-700 px-5 py-1 text-white flex-row justify-center items-center hover:bg-green-600">
-                    CONNECT
-                </button>
             </div>
         </>
     )
